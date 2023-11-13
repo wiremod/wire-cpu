3e33ded22aadf94b4014e3771792e7de44d9a27b
DerelictDrone <gage@echelonfit.com>
2023-11-12 22:43:27 -0600

    Floor main error code + fix name in opcode table

--- lua/wire/zvm/zvm_opcodes.lua
+++ lua/wire/zvm/zvm_opcodes.lua
@@ -1221,10 +1221,10 @@ ZVM.OpcodeTable[141] = function(self) --EXTRETPA
 end
 
 
-ZVM.OpcodeTable[150] = function(self) -- ERR
+ZVM.OpcodeTable[150] = function(self) -- STERR
     self:Dyn_Emit("$L P = $2")
     self:Dyn_Emit("P = P * (10^math.floor(-math.log10(math.abs(P)+1e-12)-1))")
-    self:Dyn_Emit("VM:SignalError($1+P)")
+    self:Dyn_Emit("VM:SignalError(math.floor($1)+P)")
 end
 
 ZVM.OpcodeTable[151] = function(self) -- CLERR
